72d93ac7f3a48ffed7beda0f5ad5319094f9fb6b
Gary Kertis <gary.kertis@pubmatic.com>
2015-04-29 10:01:53 -0400

    Move teeth to ngrepeat

--- app/mouth/mouth.directive.js
+++ app/mouth/mouth.directive.js
@@ -1,9 +1,13 @@
 "use strict";
 
 angular.module('gordonApp.face').directive("mouth", function() {
+
     return {
     	scope: {
     		isolatedSentence: '=sentence'
+    	},
+    	link: function($scope) {
+    		$scope.teeth = [[0,1,2,3,4,5],[6,7,8,9,10,11]];
     	},
         restrict: "E",
         templateUrl: "app/mouth/mouth.directive.html",
